95d02df156a76a1bd4bccc65c6378e6e40577f01
Joris Wachsmuth <joris.wachsmuth@uni-bielefeld.de>
2023-07-05 17:52:40 +0200

    Implemented Page Generators TODO: Fix undefined state in output

--- vhdl/tb/teletext_generator_tb.vhd
+++ vhdl/tb/teletext_generator_tb.vhd
@@ -4,6 +4,8 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use IEEE.NUMERIC_STD.ALL;
+use work.types_pkg.all;
 
 entity teletext_generator_tb is
 end teletext_generator_tb;
@@ -11,16 +13,34 @@ end teletext_generator_tb;
 architecture tb of teletext_generator_tb is
 
     component teletext_generator
-        port (CLK_IN   : in std_logic;
-              RESET_N  : in std_logic;
+        port (CLK_IN              : in    std_logic;
+              RESET_N             : in    std_logic;
+              TELETEXT_FRAME      : in    TELETEXT_FRAME;
+              PAGE_CONTROL_BITS   : in    CONTROL_BITS;
+              PAGE_NUMBER         : in    unsigned (7 downto 0);
               DATA_OUT : out std_logic;
               SYNC_OUT : out std_logic);
     end component;
 
     signal CLK_IN   : std_logic;
     signal RESET_N  : std_logic;
+    
     signal DATA_OUT : std_logic;
     signal SYNC_OUT : std_logic;
+
+    constant TELETEXT_FRAME : TELETEXT_FRAME := (others => (others => "1100111"));
+    constant PAGE_CONTROL_BITS : CONTROL_BITS := (
+        ERASE_PAGE => '0',
+        NEWSFLASH => '0',
+        SUBTITLE => '0',
+        SUPPRESS_HEADER => '0',
+        UPDATE_INDICATOR => '0',
+        INTERRUPTED_SEQUENCE => '0',
+        INHIBIT_DISPLAY => '0',
+        MAGAZINE_SERIAL => '0',
+        NATIONAL_OPTION_CHARACTER_SUBSET => "000"
+    );
+    constant PAGE_NUMBER : unsigned (7 downto 0) := "00010001";
 
     constant TbPeriod : time := (1000 ns) / 7;
     signal TbClock : std_logic := '0';
@@ -29,10 +49,13 @@ architecture tb of teletext_generator_tb is
 begin
 
     dut : teletext_generator
-    port map (CLK_IN   => CLK_IN,
-              RESET_N  => RESET_N,
-              DATA_OUT => DATA_OUT,
-              SYNC_OUT => SYNC_OUT);
+    port map (CLK_IN            => CLK_IN,
+              RESET_N           => RESET_N,
+              TELETEXT_FRAME    => TELETEXT_FRAME,
+              PAGE_CONTROL_BITS => PAGE_CONTROL_BITS,
+              PAGE_NUMBER       => PAGE_NUMBER,
+              DATA_OUT          => DATA_OUT,
+              SYNC_OUT          => SYNC_OUT);
 
     -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
--- vhdl/teletext_generator.vhd
+++ vhdl/teletext_generator.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.types_pkg.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -33,11 +34,14 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity teletext_generator is
     port(
-        CLK_IN          : in    std_logic;
-        RESET_N         : in    std_logic;
+        CLK_IN              : in    std_logic;
+        RESET_N             : in    std_logic;
+        TELETEXT_FRAME      : in    TELETEXT_FRAME;
+        PAGE_CONTROL_BITS   : in    CONTROL_BITS;
+        PAGE_NUMBER         : in    unsigned (7 downto 0);
         
-        DATA_OUT        : out   std_logic;
-        SYNC_OUT        : out   std_logic
+        DATA_OUT            : out   std_logic;
+        SYNC_OUT            : out   std_logic
     );
 end teletext_generator;
 
@@ -64,13 +68,35 @@ component shift_register
 component packet_header_generator
     port(
         MAGAZINE: in unsigned(2 downto 0);
-        PACKET: in unsigned(3 downto 0);
+        PACKET: in unsigned(4 downto 0);
         PACKET_HEADER_DATA: out std_logic_vector(39 downto 0)
     );
 end component;
 
-signal packet_trigger   : std_logic;
-signal teletext_packet  : std_logic_vector (359 downto 0) := (others => '1');
+component packet_normal_generator
+    port(
+        DATA_BYTES: in TELETEXT_ARRAY(39 downto 0);
+        PACKET_DATA: out STD_LOGIC_VECTOR(319 downto 0)
+    );
+end component;
+
+component packet_page_header_generator
+    port(
+        PAGE_NUMBER : in unsigned (7 downto 0);
+        PAGE_SUB_CODE : in unsigned (12 downto 0);
+        CONTROL_BITS : in CONTROL_BITS;
+        DATA_BYTES : in TELETEXT_ARRAY (31 downto 0);
+        PACKET_DATA : out STD_LOGIC_VECTOR (319 downto 0)
+    );
+end component;
+
+signal packet_trigger               : std_logic;
+signal teletext_packet              : std_logic_vector (359 downto 0) := (others => '1');
+signal teletext_normal_data         : std_logic_vector (319 downto 0);
+signal teletext_page_header_data    : std_logic_vector (319 downto 0); 
+signal current_line                 : unsigned (4 downto 0) := (others => '0');
+signal next_line                    : unsigned (4 downto 0) := (others => '0');
+signal current_teletext_data_line   : TELETEXT_ARRAY (39 downto 0);
 
 begin
     sync_gen : sync_generator
@@ -90,11 +116,59 @@ begin
         DATA_OUT    => DATA_OUT);
         
         
-    packet_gen : packet_header_generator
+    packet_header_gen : packet_header_generator
     port map(
         MAGAZINE => "101",
-        PACKET => "0110",
+        PACKET => current_line,
         PACKET_HEADER_DATA => teletext_packet(39 downto 0)
     );
+    
+    packet_normal_gen : packet_normal_generator
+    port map(
+        DATA_BYTES => current_teletext_data_line,
+        PACKET_DATA => teletext_normal_data
+    );
+    
+    packet_page_header_gen : packet_page_header_generator
+    port map(
+        PAGE_NUMBER => PAGE_NUMBER,
+        PAGE_SUB_CODE => (others => '0'),
+        CONTROL_BITS => PAGE_CONTROL_BITS,
+        DATA_BYTES => current_teletext_data_line(39 downto 8),
+        PACKET_DATA => teletext_page_header_data
+    );
+    
+    reg_p: process (CLK_IN)
+    begin
+        if rising_edge(CLK_IN) then
+            if RESET_N = '0' then
+                current_line <= (others => '0');
+            else
+                current_line <= next_line;
+            end if;
+        end if;
+    end process;
+    
+    advance_line: process (packet_trigger, current_line)
+    begin
+        if packet_trigger = '1' then
+            if current_line >= 24 then
+                next_line <= (others => '0');
+            else
+                next_line <= current_line + 1;
+            end if;
+        else
+            next_line <= current_line;
+        end if;
+    end process;
+    
+    switch_generator: process (current_line, teletext_page_header_data, teletext_normal_data)
+    begin
+        if current_line = 0 then
+            teletext_packet(359 downto 40) <= teletext_page_header_data;
+        else
+            teletext_packet(359 downto 40) <= teletext_normal_data;
+        end if;
+    end process;
     
 end Behavioral;
