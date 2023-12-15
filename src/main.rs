0de773182917ab3882d05bcffcfcba6b5d13c38d
Joris Wachsmuth <joris.wachsmuth@uni-bielefeld.de>
2023-12-15 15:29:08 +0100

    implemented basic teletext printing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![no_std]
 #![no_main]
 
+mod teletext;
+use teletext::TeletextInterface;
+
 use core::fmt::Write;
 use litex_hal::prelude::*;
-use litex_basys3_pac::{riscv_rt::entry, Peripherals};
+use litex_basys3_pac::{riscv_rt::entry, Peripherals, mem_map};
 use panic_halt as _;
+use crate::teletext::TeletextChar;
 
 litex_hal::uart! {
     Uart: litex_basys3_pac::UART,
@@ -20,13 +24,16 @@ const SYSTEM_CLOCK_FREQUENCY: u32 = 100_000_000;
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let mut uart = Uart::new(peripherals.UART);
+    let teletext_interface =
+        unsafe { teletext::RawTeletextInterface::new(mem_map::TELETEXT_ORIGIN) };
     writeln!(uart, "Peripherals initialized").unwrap();
 
     let mut timer = Timer::new(peripherals.TIMER0, SYSTEM_CLOCK_FREQUENCY);
-    let mut uptime = 0;
+    let mut uptime:u32 = 0;
     loop {
         timer.delay_ms(1000_u32);
         uptime += 1;
+        teletext_interface.write_char(TeletextChar(((uptime % 0x60) as u8) + 0x20), 0, 5);
         writeln!(uart, "Uptime: {} seconds", uptime).unwrap();
     }
 }
