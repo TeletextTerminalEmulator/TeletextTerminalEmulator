4cdfd3d6636a648fccdd06c83e62f501d7921abd
jorolf <jorolf@gmx.de>
2024-02-18 20:47:38 +0100

    start implementation of terminal parser

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,25 @@
 
 extern crate alloc;
 
-mod teletext_interface;
-mod teletext;
 mod character_set;
 mod error;
+mod teletext;
+mod teletext_interface;
 mod teletext_terminal;
 
+use crate::teletext::{Teletext, TeletextDimensions};
+use crate::teletext_terminal::TeletextTerminalListener;
+use alacritty_terminal::sync::FairMutex;
+use alacritty_terminal::term::Config;
+use alacritty_terminal::vte::ansi;
+use alacritty_terminal::Term;
+use teletext_terminal::LitexTimeout;
 use core::fmt::Write;
 use core::panic::PanicInfo;
-use alacritty_terminal::sync::FairMutex;
+use embedded_alloc::Heap;
+use litex_basys3_pac::{riscv_rt::entry, Peripherals};
 use litex_hal::nb::block;
 use litex_hal::prelude::*;
-use litex_basys3_pac::{riscv_rt::entry, Peripherals};
-use crate::teletext::{Teletext, TeletextDimensions};
-//#[allow(unused_imports)]
-//use panic_halt as _;
-use embedded_alloc::Heap;
-use alacritty_terminal::Term;
-use alacritty_terminal::term::Config;
-use alacritty_terminal::vte::ansi::Handler;
-use crate::teletext_terminal::TeletextTerminalListener;
 
 litex_hal::uart! {
     Uart: litex_basys3_pac::UART,
@@ -37,11 +36,24 @@ const SYSTEM_CLOCK_FREQUENCY: u32 = 100_000_000;
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
 
-static UART: FairMutex<Option<Uart>> = FairMutex::new(None);
+pub static DEBUG_UART: FairMutex<Option<Uart>> = FairMutex::new(None);
+
+macro_rules! lock_debug_uart {
+    () => {
+        DEBUG_UART
+            .lock_unfair()
+            .as_mut()
+            .expect("The DEBUG UART interface should have been initialized!")
+    };
+}
 
 macro_rules! lock_uart {
     () => {
-        UART.lock().as_mut().expect("The UART interface should have been initialized!")
+        // TODO: Replace with seperate interface?
+        DEBUG_UART
+            .lock()
+            .as_mut()
+            .expect("The UART interface should have been initialized!")
     };
 }
 
@@ -57,33 +69,29 @@ fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let uart = Uart::new(peripherals.UART);
 
-    *UART.lock_unfair() = Some(uart);
+    *DEBUG_UART.lock_unfair() = Some(uart);
 
     let teletext = unsafe { Teletext::new_raw() };
-    writeln!(lock_uart!(), "Peripherals initialized").unwrap();
+    writeln!(lock_debug_uart!(), "Peripherals initialized").unwrap();
 
     let term_config = Config::default();
 
-    let _term = Term::new(term_config, &TeletextDimensions, TeletextTerminalListener);
+    let mut term = Term::new(
+        term_config,
+        &TeletextDimensions,
+        TeletextTerminalListener(teletext),
+    );
 
-    
-    for (col, ch) in "Hallo Welt!".chars().enumerate() {
-        teletext.set_char(ch, 7, col as u8).expect("Values should be fine");
-    }
+    let mut parser = ansi::Processor::<LitexTimeout>::default();
+
     loop {
-        let ch = block!(lock_uart!().read()).expect("Infallible");
-
-        teletext.set_char(ch as char, 8, 0).or_else( |error| {
-                teletext.set_char('?', 8, 0)
-            }
-        ).expect("Should be handled");
-        block!(lock_uart!().write(ch)).expect("Infallible");
+        let input = block!(lock_uart!().read()).expect("Infallible");
+        parser.advance(&mut term, input);
     }
-
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    writeln!(lock_uart!(), "{}", info).unwrap();
+    writeln!(lock_debug_uart!(), "{}", info).unwrap();
     loop {}
 }
--- src/teletext_terminal.rs
+++ src/teletext_terminal.rs
@@ -1,13 +1,38 @@
-use alacritty_terminal::event::{Event, EventListener};
+use core::iter::repeat;
 
-pub struct TeletextTerminalListener;
+use alacritty_terminal::{event::{Event, EventListener}, vte::ansi::Timeout};
 
-impl EventListener for TeletextTerminalListener {
+use crate::{
+    teletext::{self, Teletext},
+    teletext_interface::TeletextInterface,
+};
+
+const HEADER_OFFSET: u8 = 8;
+const HEADER_LENGTH: u8 = teletext::COLUMN_COUNT - HEADER_OFFSET;
+
+pub struct TeletextTerminalListener<T: TeletextInterface>(pub Teletext<T>);
+
+impl<T: TeletextInterface> EventListener for TeletextTerminalListener<T> {
     fn send_event(&self, event: Event) {
         match event {
             Event::MouseCursorDirty => {}
-            Event::Title(_) => {}
-            Event::ResetTitle => {}
+            Event::Title(title) => {
+                let title_line = title
+                    .chars()
+                    .chain(repeat(' '))
+                    .take(HEADER_LENGTH as usize);
+
+                self.0
+                    .set_line(title_line, 0, HEADER_OFFSET, Some('?'))
+                    .expect("Title line should never exceed bounds");
+            }
+            Event::ResetTitle => {
+                let reset_title = repeat(' ').take(HEADER_LENGTH as usize);
+
+                self.0
+                    .set_line(reset_title, 0, HEADER_OFFSET, Some('?'))
+                    .expect("Resetting the title should never fail");
+            }
             Event::ClipboardStore(_, _) => {}
             Event::ClipboardLoad(_, _) => {}
             Event::ColorRequest(_, _) => {}
@@ -16,7 +41,26 @@ impl EventListener for TeletextTerminalListener {
             Event::CursorBlinkingChange => {}
             Event::Wakeup => {}
             Event::Bell => {}
-            Event::Exit => {}
+            Event::Exit => unimplemented!(),
         }
+    }
+}
+
+
+#[derive(Default)]
+pub struct LitexTimeout;
+
+impl Timeout for LitexTimeout {
+
+    fn set_timeout(&mut self, duration: core::time::Duration) {
+        todo!()
+    }
+
+    fn clear_timeout(&mut self) {
+        todo!()
+    }
+
+    fn pending_timeout(&self) -> bool {
+        todo!()
     }
 }
